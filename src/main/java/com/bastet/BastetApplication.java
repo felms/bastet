6c17a6cb96736b655ada02dbc6ff0f3a6d077252
Felipe <felipemoreira1@gmail.com>
2022-04-02 22:24:28 -0300

    Initial commit

---
+++ src/main/java/com/bastet/BastetApplication.java
@@ -0,0 +1,13 @@
+package com.bastet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BastetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BastetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bastet/BastetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bastet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BastetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
